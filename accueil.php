1ff2f5aa0092d1de79ea947e18c8ed0eb295cbeb
gabriel <perinogabriel1@gmail.com>
2021-03-24 16:48:52 +0100

    html css site

---
+++ accueil.php
@@ -0,0 +1,10 @@
+<head>
+<title>accueil</title>
+</head>
+<?php
+include 'header.php';
+?>
+<body>
+    
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+    <style>
+        .titre
+        {
+            background-color: blue;
+            color: white;
+        }
+    </style>
+    <div class="titre"> <h1>AU BON MARCHE</h1></div>
+</header>
