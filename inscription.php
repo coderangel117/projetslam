19b4875a6dd4f9f6a00e6235198ebf225879e877
gabriel <perinogabriel1@gmail.com>
2021-03-26 12:04:48 +0100

    modif inscription.php
    +test mdplength
    +test pseudoexist

--- inscription.php
+++ inscription.php
@@ -1,95 +1,7 @@
- <?php
-/*
-include'connexion.php';
-if(true)
-{
-    if (!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mdp']))
-    {
-        $pseudo = htmlspecialchars($_POST['pseudo']);
-        $mail = htmlspecialchars($_POST['mail']);
-        $mdp = password_hash($_POST['mdp']);
-        var_dump($pseudo);
-        var_dump($mail);
-        var_dump($mdp);
-
-    
-        $pseudolenght = strlen($pseudo);
-        if($pseudolenght >5 ) 
-        {
-           
-            if(filter_var($mail, FILTER_VALIDATE_EMAIL))
-                
-                {
-                    $insertmbr = $bdd->prepare("INSERT INTO membres('pseudo', 'mail', 'mot de passe') values (?,?,?)");
-                    $insertmbr->execute(array($pseudo, $mail, $mdp));
-                    header('Location: accueil.html');
-                }
-        }
-        else
-        {
-            $erreur="votre pseudo ne doit pas être inferieur à 2 caractères !";
-            echo $erreur;
-        }
-    }
-    else
-    {
-        $erreur="des champs ne sont pas complétés";
-        echo $erreur;
-    }
-}
-else
-{
-    echo 'false';
-}
-?>
-
-
-
-<body>
-
-    <h2>inscription</h2>
-    <form method="POST" action="inscription.php">
-
-    <table>
-        <tr>
-            <td>
-            <label for="pseudo" > pseudo :</label>
-            </td>
-            <td>
-                <input type="text" placeholder="votre pseudo" name="Pseudo" id="pseudo" value="">
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <label for="mail"> adresse mail :</label>
-            </td>
-            <td>
-                <input type="email" placeholder="votre adresse mail" name="Mail" id="mail"  value="" >
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <label for="mdp"> mot de passe :</label>
-            </td>
-            <td>
-                <input type="password" placeholder="votre mot de passe" name="Mdp" id="mdp" >
-            </td>
-        </tr>
-        <tr>
-            <td ></td>
-            <td><input type="submit"  value="je m'inscris"></td>
-        </tr>
-    </table>
-    </form>
-</body>
-*/
-
-?>
-
-
-
 <?php
 include 'connexion.php';
+
+include 'header.php';
 if(isset($_POST['forminscription'])) {
    $pseudo = htmlspecialchars($_POST['pseudo']);
    $mail = htmlspecialchars($_POST['mail']);
@@ -99,27 +11,41 @@ if(isset($_POST['forminscription'])) {
    if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
       $pseudolength = strlen($pseudo);
       if($pseudolength <= 255) {
-         if($mail == $mail2) {
-            if(filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-               $reqmail = $connexion->prepare("SELECT * FROM membres WHERE mail = ?");
-               $reqmail->execute(array($mail));
-               $mailexist = $reqmail->rowCount();
-               if($mailexist == 0) {
-                  if($mdp == $mdp2) {
-                     $insertmbr = $connexion->prepare("INSERT INTO membres(pseudo, mail, motdepasse) VALUES(?, ?, ?)");
-                     $insertmbr->execute(array($pseudo, $mail, $mdp));
-                     $erreur = "Votre compte a bien été créé ! <a href=\"connexionutilisateur.php\">Me connecter</a>";
+         $reqpseudo = $connexion->prepare("SELECT * FROM membres WHERE pseudo = ?");
+         $reqpseudo->execute(array($pseudo));
+         $pseudoexist = $reqpseudo->rowCount();
+         if($pseudoexist == 0) {
+            if($mail == $mail2) {
+               if(filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+                  $reqmail = $connexion->prepare("SELECT * FROM membres WHERE mail = ?");
+                  $reqmail->execute(array($mail));
+                  $mailexist = $reqmail->rowCount();
+                  if($mailexist == 0) {
+                     $mdplength = strlen($mdp);
+                     if($mdplength>8){
+                        
+                           if($mdp == $mdp2) {
+                              $insertmbr = $connexion->prepare("INSERT INTO membres(pseudo, mail, motdepasse) VALUES(?, ?, ?)");
+                              $insertmbr->execute(array($pseudo, $mail, $mdp));
+                              $erreur = "Votre compte a bien été créé ! <a href=\"connexionutilisateur.php\">Me connecter</a>";
+                           } else {
+                              $erreur = "Vos mots de passes ne correspondent pas !";
+                           }
+
+                     }else{
+                        $erreur = "mot de passe trop  petit pour être sécurisé";
+                     }
                   } else {
-                     $erreur = "Vos mots de passes ne correspondent pas !";
+                     $erreur = "Adresse mail déjà utilisée !";
                   }
                } else {
-                  $erreur = "Adresse mail déjà utilisée !";
-               }
+                  $erreur = "Votre adresse mail n'est pas valide !";
+               } 
             } else {
-               $erreur = "Votre adresse mail n'est pas valide !";
+               $erreur = "Vos adresses mail ne correspondent pas !";
             }
-         } else {
-            $erreur = "Vos adresses mail ne correspondent pas !";
+         } else{
+            $erreur = "Pseudo déja utilisé !";
          }
       } else {
          $erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
