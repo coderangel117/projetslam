1ab47edc0d5260ffba5756aae78141a7e9bb82e9
gabriel <perinogabriel1@gmail.com>
2021-05-14 15:40:15 +0200

    SELECT CLIENT OPERATIONNEL
    pas de css appliqué mais dans un tableau sans bordure

--- selectclient.php
+++ selectclient.php
@@ -1,32 +1,37 @@
 <?php
 include'headerstock.php';
+
 ?>
 
 <h1>Bases de données MySQL</h1>  
-<?php
-    $servname = "localhost"; $dbname = "projetslam"; $user = "test"; $pass = "mdp";
-    
-    try{
-        /*parametres connexion vers bdd */
-        $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
-        $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+<div class="search">
+<input type="search" name="search" id="search"  placeholder="search" value="search">
+</div>
 
-        $sth = $dbco->prepare("SELECT  prenom, nom FROM client WHERE nom like '%P%'");
-        $sth->execute();
-        
-        /*Retourne un tableau associatif pour chaque entrée de notre table
-         *avec le nom des colonnes sélectionnées en clefs*/
-        $resultat = $sth->fetchAll(PDO::FETCH_ASSOC);
-        
-        /*print_r permet un affichage lisible des résultats,
-         *<pre> rend le tout un peu plus lisible*/
-        echo '<pre>';
-        print_r($resultat);
-        echo '</pre>';
-    }
-          
-    catch(PDOException $e){
-        echo "Erreur : " . $e->getMessage();
-    }
+<?php
+ 
+$pdo = new PDO('mysql:dbname=projetslam;host=localhost', 'test','mdp',array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES\'UTF8\''));
+
+$selectclient = $pdo->query('SELECT prenom, nom FROM client');
+
 ?>
+<div class="tableau" >
+<?php
+echo '<table>
+
+<tr><td><p>prenom</p></td><td><p>nom</p></td></tr><tr>';
+
+while ($tableau = $selectclient->fetch())
+{
+?>
+<table >
+
+<?php
+echo ('  <tr><td>' . $tableau['prenom'] . ' ' .'</td><td>' . $tableau['nom']. '</td></tr></table>') ;
+}
+?></table>
+
+</div>
+
+
+
