dd94f798db0a0f25d443499059209cfd541d61a2
gabriel <perinogabriel1@gmail.com>
2021-05-14 23:22:13 +0200

    Update deleteclient.php

--- deleteclient.php
+++ deleteclient.php
@@ -2,16 +2,17 @@
 include'headerstock.php';
 include'connexionstock.php';
 
-
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
+if(isset($_POST['nom']) && isset($_POST['prenom'])){
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+}
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 <?php
-$deleteclient = $connexion->query("DELETE prenom, nom FROM client where prenom='.$prenom.'and nom=.$nom");
+$deleteclient = $connexion->query('DELETE prenom, nom FROM client where prenom='.$prenom.'and nom=.$nom');
 
 ?>
-<form method="POST" action=""  onsubmit="return confirmation()";>
+<form method="POST" action="" > <!--- onsubmit="return confirmation()" -->
     <div class="titreform"> 
         prenom client 
     </div>
