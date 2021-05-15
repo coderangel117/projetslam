7107be11749847a73dde7f4c1547c331d9ee3c08
gabriel <perinogabriel1@gmail.com>
2021-05-15 11:42:01 +0200

    Update updateclient.php
    remplacement variable adaptée

--- updateclient.php
+++ updateclient.php
@@ -2,18 +2,14 @@
 include'connexionstock.php';
 include'headerstock.php';
 
+$ancienprenom = $_POST['anciennom'];
 $anciennom = $_POST['anciennom'];
-$ancienprixv = $_POST['ancienprixv'];
-$ancienprixa = $_POST['ancienprixa'];
-$anciennequantite = $_POST['anciennequantite'];
 
-$nouveaunom = $_POST['nouveaunom'];
-$nouveauprixv= $_POST['nouveauprixv'];
-$nouveauprixa = $_POST['nouveauprixa'];
-$nouvellequantite= $_POST['nouvellequantite'];
+$nouveauprenom = $_POST['nouveauprenom'];
+$nouveaunom= $_POST['nouveaunom'];
 
 
-$insert = $connexion->prepare('UPDATE fournisseur SET nomproduit=$anciennom, prixvente= $ancienprixv ,prixachat=$ancienprixa, quantitestock= $anciennequantite where quantitestock='.$quantite);
+$insert = $connexion->prepare('UPDATE client SET prenom=$nouveauprenom, nom=$nouveaunom where prenom=$ancienprenom'  );
 $message = "les données ont bien été enregistrées ! ";
 
 ?>
@@ -25,47 +21,24 @@ $message = "les données ont bien été enregistrées ! ";
 <table>
     <tr>
         <td>
-            nom du produit a remplacer<input type="text" class="form-control" placeholder="saisissez le nom du produit à remplacer "  id="anciennom" name="anciennom" value="<?php if(isset($anciennom)) { echo $anciennom; } ?>">
+            prenom du client a remplacer<input type="text" class="form-control" placeholder="saisissez le nom du produit à remplacer "  id="ancienprenom" name="ancienprenom" value="<?php if(isset($ancienprenom)) { echo $ancienprenom; } ?>">
             <br>
         </td>
         <td>
-        nouveau nom du produit
+        nouveau prenom du client
         <input type="text" class="form-control" placeholder="saisissez le nom du produit choisi"  id="nouveaunom" name="nouveaunom" value="<?php if(isset($nouveaunom)) { echo $nouveaunom; } ?>">
         <br>
         </td>
     </tr>
     <tr>
         <td>
-        prix de vente a remplacer
-        <input type="text" class="form-control" placeholder="saisissez le prix de vente du produit à remplacer" id="ancienprixv" name="ancienprixv" value="<?php if(isset($ancienprixv)) { echo $ancienprixv; } ?>">
+       nom a remplacer
+        <input type="text" class="form-control" placeholder="saisissez le nom du client du produit à remplacer" id="anciennom" name="anciennom" value="<?php if(isset($anciennom)) { echo $anciennom; } ?>">
         <br> 
         </td>
         <td>
-        nouveau prix de vente
-        <input type="text" class="form-control" placeholder="saisissez le prix de vente du produit choisi" id="nouveauprixv" name="nouveauprixv" value="<?php if(isset($nouveauprixv)) { echo $nouveauprixv; } ?>">
-        <br>
-        </td>
-    </tr>
-    <tr>
-        <td>
-        prix d'achat à remplacer
-        <input type="text" class="form-control" placeholder="saisissez le prix d'achat du produit à remplacer" id="ancienprixa" name="ancienprixa" value="<?php if(isset($ancienprixa)) { echo $ancienprixa; } ?>">
-        <br>
-        </td>
-        <td>
-        nouveau prix d'achat 
-        <input type="text" class="form-control" placeholder="saisissez le prix d'achat du produit choisi" id="nouveauprixva" name="nouveauprixa" value="<?php if(isset($nouveauprixa)) { echo $nouveauprixa; } ?>">
-        <br>
-        </td>
-    </tr>
-    <tr>
-        <td>
-        quantite à remplacer<input type="text" class="form-control" placeholder="saisissez la quantité de ce produit à remplacer" id="anciennequantite" name="anciennequantite" value="<?php if(isset($anciennequantite)) { echo $anciennequantite; } ?>">
-        <br>
-        </td>
-        <td>
-        nouvelle quantité de produit 
-        <input type="text" class="form-control" placeholder="saisissez la quantité de ce produit choisi" id="nouvellequantite" name="nouvellequantite" value="<?php if(isset($nouvellequantite)) { echo $nouvellequantite; } ?>">
+        nouveau nom du client
+        <input type="text" class="form-control" placeholder="saisissez le nom du client du produit choisi" id="nouveaunom" name="nouveaunom" value="<?php if(isset($nouveaunom)) { echo $nouveaunom; } ?>">
         <br>
         </td>
     </tr>
