9a9fda6053ef0b50f2ad02c27197eed442df7b6e
gabriel <perinogabriel1@gmail.com>
2021-05-15 10:14:52 +0200

    Update deletefournisseur.php

--- deletefournisseur.php
+++ deletefournisseur.php
@@ -2,28 +2,47 @@
 include'headerstock.php';
 include'connexionstock.php';
 
-if(isset($_POST['nom']) && isset($_POST['prenom'])){
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
+if(isset($_POST['nomfournisseur'])){
+    $nomfournisseur = $_POST['nomfournisseur'];
+} 
+if(isset($_POST['adresse'])){
+    $adresse = $_POST['adresse'];
+}
+if(isset($_POST['telephone'])){
+    $telephone = $_POST['telephone'];
+}
+if(isset($_POST['mail'])){
+    $mail = $_POST['mail'];
 }
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 <?php
-$deleteclient = $connexion->query('DELETE prenom, nom FROM client where prenom='.$prenom.'and nom=.$nom');
-
+$deletefournisseur = $connexion->query("DELETE nomfournisseur, adresse, telephone, mail FROM fournisseur where nomfournisseur='.$nomfournisseur.'and adresse=.'$adresse and telephone=.'$telephone and mail=.'$mail");
+var_dump($deletefournisseur);
 ?>
 <form method="POST" action="" > <!--- onsubmit="return confirmation()" -->
     <div class="titreform"> 
-        prenom client 
+        nom du produit
     </div>
-    <input type="text" class="form-control" placeholder="saisissez le prenom du client" id="prenom" name="prenom" value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+    <input type="text" class="form-control" placeholder="saisissez le nom du fournisseur" id="nomfournisseur" name="nomfournisseur" value="<?php if(isset($nomfournisseur)) { echo $nomfournisseur; } ?>">
     <br>
     <div class="titreform">
-        nom client
+        prix de vente
     </div>
-    <input type="text" class="form-control" placeholder="saisissez le nom du client" id="nom" name="nom" value="<?php if(isset($nom)) { echo $nom; } ?>">
+    <input type="text" class="form-control" placeholder="saisissez l'adresse du fournisseur" id="adresse" name="adresse" value="<?php if(isset($adresse)) { echo $adresse; } ?>">
+    <br>
+    
+    <div class="titreform">
+        prix d'achat
+</div>
+    <input type="text" class="form-control" placeholder="saisissez le prix d'achat" id="telephone" name="telephone" value="<?php if(isset($telephone)) { echo $telephone; } ?>">
     <br>
 
+    <div class="titreform">
+        quantite en stock 
+</div>
+    <input type="text" class="form-control" placeholder="saisissez la mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>">
+    <br>
     <button type="submit" class="btn btn-primary">Submit</button>
 
 <br>
