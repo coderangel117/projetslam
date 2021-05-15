31d4b3e30f899c07457aeb7a596c07e55eb1f9a8
gabriel <perinogabriel1@gmail.com>
2021-05-15 10:55:59 +0200

    Update updateproduit.php

--- updateproduit.php
+++ updateproduit.php
@@ -2,22 +2,19 @@
 include'connexionstock.php';
 include'headerstock.php';
 
-$anciennom = $_POST['nomproduit'];
-$ancienprixv = $_POST['prixvente'];
-$ancienprixa = $_POST['prixachat'];
-$anciennequantite = $_POST['quantite'];
+$anciennom = $_POST['anciennom'];
+$ancienprixv = $_POST['ancienprixv'];
+$ancienprixa = $_POST['ancienprixa'];
+$anciennequantite = $_POST['anciennequantite'];
 
-$nouveaunom = $_POST['nomproduit'];
-$nouveauprixv= $_POST['prixvente'];
-$nouveauprixa = $_POST['prixachat'];
-$nouvellequantite= $_POST['quantite'];
+$nouveaunom = $_POST['nouveaunom'];
+$nouveauprixv= $_POST['nouveauprixv'];
+$nouveauprixa = $_POST['nouveauprixa'];
+$nouvellequantite= $_POST['nouvellequantite'];
 
 
-if(!empty($_POST['nomproduit'])AND !empty($_POST['prixvente']) AND !empty($_POST['prixvente'])) {
-    $insert = $connexion->prepare('UPDATE fournisseur SET nomproduit=$nomproduit, prixvente= $prixvente ,prixachat=$prixachat, quantitestock= $quantite where quantitestock='.$quantite);
-    $message = "les données ont bien été enregistrées ! ";
-}
-else {$message = "Tous les champs doivent être complétés !";}
+$insert = $connexion->prepare('UPDATE fournisseur SET nomproduit=$anciennom, prixvente= $ancienprixv ,prixachat=$ancienprixa, quantitestock= $anciennequantite where quantitestock='.$quantite);
+$message = "les données ont bien été enregistrées ! ";
 
 ?>
 <head>
@@ -25,18 +22,32 @@ else {$message = "Tous les champs doivent être complétés !";}
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 </head>
 
-<form method="POST" acttion="">
+<form method="POST" action="">
+anciennomproduit
+<input type="text" class="form-control" placeholder="saisissez le nom du produit à remplacer "  id="anciennom" name="anciennom" value="<?php if(isset($anciennom)) { echo $anciennom; } ?>">
+<br>
+ancienprixv
+<input type="text" class="form-control" placeholder="saisissez le prix de vente du produit à remplacer" id="ancienprixv" name="ancienprixv" value="<?php if(isset($ancienprixv)) { echo $ancienprixv; } ?>">
+<br>ancienprixa
+<input type="text" class="form-control" placeholder="saisissez le prix d'achat du produit à remplacer" id="ancienprixa" name="ancienprixa" value="<?php if(isset($ancienprixa)) { echo $ancienprixa; } ?>">
+<br>
+anciennequantite 
+<input type="text" class="form-control" placeholder="saisissez la quantité de ce produit à remplacer" id="anciennequantite" name="anciennequantite" value="<?php if(isset($anciennequantite)) { echo $anciennequantite; } ?>">
+<br>
+
 nomproduit
-<input type="text" class="form-control" placeholder="saisissez le nom du produit"  id="nomproduit" name="nomproduit" value="<?php if(isset($nomproduit)) { echo $nomproduit; } ?>">
+<input type="text" class="form-control" placeholder="saisissez le nom du produit choisi"  id="nouveaunom" name="nouveaunom" value="<?php if(isset($nouveaunom)) { echo $nouveaunom; } ?>">
 <br>
 prix vente
-<input type="text" class="form-control" placeholder="saisissez le prix de vente du produit" id="prixvente" name="prixvente" value="<?php if(isset($prixvente)) { echo $prixvente; } ?>">
+<input type="text" class="form-control" placeholder="saisissez le prix de vente du produit choisi" id="nouveauprixv" name="nouveauprixv" value="<?php if(isset($nouveauprixv)) { echo $nouveauprixv; } ?>">
 <br>prixachat
-<input type="text" class="form-control" placeholder="saisissez le prix d'achat du produit" id="prixachat" name="prixachat" value="<?php if(isset($prixachat)) { echo $prixachat; } ?>">
+<input type="text" class="form-control" placeholder="saisissez le prix d'achat du produit choisi" id="nouveauprixva" name="nouveauprixa" value="<?php if(isset($nouveauprixa)) { echo $nouveauprixa; } ?>">
 <br>
 quantité de produit 
-<input type="text" class="form-control" placeholder="saisissez la quantité de ce produit" id="quantite" name="quantite" value="<?php if(isset($quantite)) { echo $quantite; } ?>">
+<input type="text" class="form-control" placeholder="saisissez la quantité de ce produit choisi" id="nouvellequantite" name="nouvellequantite" value="<?php if(isset($nouvellequantite)) { echo $nouvellequantite; } ?>">
 <br>
+
+
 <button type="submit" class="btn btn-primary">Submit</button>
 <?php
 if(isset($message)){
