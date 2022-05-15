c5419ee2982b4e1559853199818209fa941e1ad6
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-15 11:01:49 +0200

    [selectproduit] : just refactoring footer/header files

--- selectproduit.php
+++ selectproduit.php
@@ -1,6 +1,6 @@
 <?php
-include 'header.php';
-include 'connexion_bdd.php';
+require_once 'element/header.php';
+require_once 'connexion_bdd.php';
 ?>
 <head>
     <link rel="stylesheet" href="stylestock.css">
@@ -72,5 +72,5 @@ var_dump($selectproduit);
 
 </div>
 <?php
-include 'footer.php';
+require_once 'element/footer.php';
 ?>
