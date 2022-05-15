244f7a5d6320a9b66b185581835a45bcf67f7368
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-15 18:04:02 +0200

    [AddClient] : reformatting code

--- clients/AddClient.php
+++ clients/AddClient.php
@@ -13,33 +13,36 @@ if(!empty($_POST['nom'])AND !empty($_POST['prenom'])) {
     $insert = $connexion->prepare("INSERT INTO client (prenom, nom) VALUES(?,?)");
     $insert->execute(array($prenom, $nom, ));
     $message = "les données ont bien été enregistrées ! ";
-}
-else {$message = "Tous les champs doivent être complétés !";}
-?>
-<head>
-<title>client</title>
-
-</head>
-<form method="POST" acttion="">
-    <div class="titreform"> 
-        prenom client 
-    </div>
-    <input type="text" class="form-control" placeholder="saisissez le prenom du client" id="prenom" name="prenom" value="<?php if(isset($prenom)) { echo $prenom; } ?>">
-
-    <div class="titreform">
-        nom client
-    </div>
-    <input type="text" class="form-control" placeholder="saisissez le nom du client" id="nom" name="nom" value="<?php if(isset($nom)) { echo $nom; } ?>">
-    <br>
-
-    <br>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    <?php
-if(isset($message)){
-    echo($message);
+} else {
+    $message = "Tous les champs doivent être complétés !";
 }
 ?>
-</form>
+    <head>
+        <title>client</title>
+
+    </head>
+    <form method="POST" action="">
+        <div class="titreform">
+            Prenom
+        </div>
+        <label for="prenom">
+            saisissez le prenom du client
+        </label><input type="text" class="form-control"  id="prenom" name="prenom" value="<?php if (isset($prenom)) {echo $prenom;} ?>">
+
+        <div class="titreform">
+            Nom
+        </div>
+        <label for="nom">
+            saisissez le nom du client
+        </label>
+        <input type="text" class="form-control"  id="nom" name="nom" value="<?php if (isset($nom)) {echo $nom;} ?>">
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <?php
+        if (isset($message)) {
+            echo($message);
+        }?>
+    </form>
 <?php
-require_once __DIR__ .'/../element/footer.php';
+require_once __DIR__ . '/../element/footer.php';
 ?>
