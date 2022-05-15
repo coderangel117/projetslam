96b9d687413e97ebb158cf0639c6e272f3b36710
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-15 18:21:30 +0200

    [logout] :refactoring require

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
-require_once 'connexion_bdd.php';
+require_once __DIR__ .'/Connexion.class.php';
+require_once 'functions/sql.php';
 session_start();
 $_SESSION = array();
 session_destroy();
-header("Location: connexionutilisateur.php"); ?>
+header("Location: login.php"); ?>
