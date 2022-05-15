cb447c02ef772afd5336f00e68f549a7f2663800
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-15 18:19:58 +0200

    [produits/index] : use Query() instead of PDO functions  improve the structure of table and use foreach()instead while() / refactoring require

--- produits/index.php
+++ produits/index.php
@@ -1,76 +1,67 @@
 <?php
-require_once __DIR__ .'/../element/header.php';
-require_once __DIR__ .'/../connexion_bdd.php';
+require_once __DIR__ . '/../element/header.php';
+require_once __DIR__ . '/../Connexion.class.php';
 ?>
-<head>
-    <link rel="stylesheet" href="stylestock.css">
-</head>
+    <head>
+        <link rel="stylesheet" href="stylestock.css">
+    </head>
 
-<h1>Bases de données MySQL</h1>  
-<div class="search">
-<input type="search" name="search" id="search"  placeholder="search" value="search">
-</div>
+    <h1>Bases de données MySQL</h1>
+    <div class="search">
+        <input type="search" name="search" id="search" placeholder="search" value="search">
+    </div>
 
 <?php
- 
-$selectproduit = $connexion->query('SELECT idproduit, nomproduit, prixvente, prixachat, quantitestock FROM produits');
-var_dump($selectproduit);
+$sql = 'SELECT IdProduit, IdRayon, NomProduit, PrixVente, PrixAchat, QuantiteStock from projetslam.produits ';
+$selectproduit = Query($sql, []);
 ?>
-<div class="collapse" >
-    <?php
-    echo '<table>
-
-        <tr>
-            <td>
-                <p>
-                 id du produit
-                </p>
-            </td>
-            <td>
-                <p>
-                    nom du produit
-                </p>
-            </td>
-            <td>
-                <p>
-                    prix de vente 
-                </p>
-            </td>
-            <td>
-                <p>
-                    prix d\'achat 
-                </p>
-            <div class="quantite">
+    <div class="collapse">
+        <table>
+            <tr>
                 <td>
                     <p>
-                        quantité en stock            
+                        id du produit
                     </p>
-                </td>            
-            </div>
-        </tr>';
-        while ($tableau = $selectproduit->fetch())
-        {
-        ?>
-        <table >
-
-        <?php
-        echo ('
-        <tr>
-            <td>' . $tableau['idproduit'] . '</td>
-            <td>' . $tableau['nomproduit'] . '</td>
-            <td>' . $tableau['prixvente'] . '</td>
-            <td>' . $tableau['prixachat'] .'</td>
-
-            <div class="quantite">
-                <td>' . $tableau['quantitestock'] .'</td>
-            </div>
-            
-        </tr>
-    </table>') ;
-    }
-    ?></table>
-
-</div>
+                </td>
+                <td>
+                    <p>
+                        id du rayon
+                    </p>
+                </td>
+                <td>
+                    <p>
+                        nom du produit
+                    </p>
+                </td>
+                <td>
+                    <p>
+                        prix de vente
+                    </p>
+                </td>
+                <td>
+                    <p>
+                        prix d'achat
+                    </p>
+                <td>
+                    <p>
+                        quantité en stock
+                    </p>
+                </td>
+            </tr>
+            <?php
+            foreach ($selectproduit as $produits) { ?>
+            <tr>
+                <td><?= $produits['IdProduit']; ?></td>
+                <td><?= $produits['IdRayon']; ?></td>
+                <td><?= $produits['NomProduit']; ?></td>
+                <td><?= $produits['PrixVente']; ?></td>
+                <td><?= $produits['PrixAchat']; ?></td>
+                <td><?= $produits['QuantiteStock']; ?></td>
+                <?php }
+            ?>
+            </tr>
+        </table>
+    </div>
 <?php
-require_once __DIR__ .'/../element/footer.php';
+require_once __DIR__ . '/../element/footer.php';
 ?>
