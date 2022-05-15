dd6f11da2c6d446050c9694b1e7e03bde18d50df
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-15 12:13:07 +0200

    [/header] : just refactoring file name in links

--- element/header.php
+++ element/header.php
@@ -13,11 +13,11 @@
                 <li class="deroulant">menu
                     <ul class="sous">
                         <li><a href="../../index.php">revenir à l'accueil</a></li>
-                        <li><a href="../connexionutilisateur.php">se connecter</a></li>
+                        <li><a href="../login.php">se connecter</a></li>
                     </ul>
                     <?php
                     if (isset($_SESSION) && !empty($_SESSION)) {?>
-                        <li><a href="../accueilstocks.php"> Menu principal de l'application</a></li>
+                        <li><a href="../menu.php"> Menu principal de l'application</a></li>
                 <?php } ?>
                 </li>
             </ul>
