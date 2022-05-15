3eceed5802082af52baf94010499ecc218e0dec5
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-15 11:36:37 +0200

    [gestionstock] : remove poop code bppstrap link

--- gestionstocks.php
+++ gestionstocks.php
@@ -6,15 +6,13 @@ require_once 'element/header.php'; ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0"
-              crossorigin="anonymous">
+
         <title>Document</title>
     </head>
     <body>
 
 
-    EFFECTUER DES Entrée DE PRODUITS SUR LA BASE DE DONNEES
+    EFFECTUER DES ENTRÉE DE PRODUITS SUR LA BASE DE DONNEES
     <form method="POST">
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label"> </label>
