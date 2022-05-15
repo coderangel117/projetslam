c94a38f8791b16dfbae224d14a7bac53c7b1cde5
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-15 18:23:14 +0200

    [/functions] : just add the files from stage_canoe's project / split auth.php to 2 files

---
+++ functions/sql.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__. '/../Connexion.class.php';
+/**
+ * @param string $sql // represents the SQL query
+ * @param mixed $parameters // represents the parameters of the query if not empty else type
+ * @param string $type // represents the type of SQL query among the 4 propositions : select /update / insert / delete
+ * @return bool|PDOStatement|void
+ */
+function Query(string $sql, array $parameters =[], string $type = "select")
+{
+    if (isset($_SESSION) && !empty($_SESSION)) {
+            $connexion_admin = new Connexion("root", "");
+            $connexion = $connexion_admin->getConnexion();
+            var_dump($connexion);
+        if ($type !== "select") {
+            $requete = $connexion->prepare($sql);
+            return $requete->execute($parameters);
+        } else {
+            if ($parameters == []) {
+                return $connexion->query($sql);
+            } else {
+                try {
+                    $requete = $connexion->prepare($sql);
+                    $requete->execute($parameters);
+                    return $requete;
+                } catch (Exception $e) {
+                    echo 'there is a problem with the select parameters' . $e->getMessage();
+                }
+            }
+        }
+    } else {
+        die("pas de session pas de requete ...");
+    }
+}
